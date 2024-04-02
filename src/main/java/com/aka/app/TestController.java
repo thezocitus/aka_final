08b9002937fc6a4bead6d2533613ee40148aa8e4
dirokim <dirokim7@gmail.com>
2024-04-02 16:58:56 +0900

    feat setting

--- src/main/java/com/aka/app/TestController.java
+++ src/main/java/com/aka/app/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
 	@GetMapping("/")
 	public String test (Model model) {
-		return "html/index";
+		return "temp/sample";
 	}
+	
 }
