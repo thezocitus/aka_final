c3890ec4eaa59255ff3c0046c9dc1ed870654038
otlb <an95621@naver.com>
2024-04-04 09:28:03 +0900

    add

--- src/main/java/com/aka/app/util/Pager.java
+++ src/main/java/com/aka/app/util/Pager.java
@@ -12,164 +12,105 @@ import lombok.ToString;
 @Component
 public class Pager {
 	
-	//페이지
-	private Long startRow;
-	private Long lastRow;
-	private Long page;
-	private Long perPage=10L;
+	private Long page;//페이지 번호
+	private Long perPage; //한페이지당 출력 갯수
 	
-	//블럭
+	private Long startIndex;
+	
+	
 	private Long totalPage;
 	private Long startNum;
 	private Long lastNum;
 	
-	//boolean
+	//이전 블럭이 없으면 true;
 	private boolean start;
+	//다음 블럭이 없으면 true;
 	private boolean last;
 	
+	//검색 관련
 	private String search;
 	private String kind;
 	
 	
-	public void makeRow() {
-		this.startRow = ((this.getPage()-1)*this.getPerPage())+1;
-		this.lastRow =  this.getPage()*this.getPerPage();
-	
+	public void makeIndex() {
+		//1 0
+		//2 10
+		//3 20
+		this.startIndex= (this.getPage()-1)*this.getPage();
+		
 	}
 	
-	public void makePage(Long totalCount) {
+	public void makeNum(Long totalCount) {
+		if(totalCount<1) {
+			totalCount=1L;
+		}
+		
 		totalPage = totalCount/this.getPerPage();
-		if(totalCount%this.getPerPage()!=0) {
-		totalPage++;
+		
+		
+		if(totalCount%this.getPerPage() != 0) {
+			//totalPage=totalPage+1;
+			totalPage++;
 		}
 		
-			
-		Long totalBlock = 0L;
-		Long perBlock = 5L;
-		totalBlock = totalPage/perBlock;
-		if(totalPage%perBlock!=0) {
-		totalBlock++;
+		this.setTotalPage(totalPage);
+		
+		//2. 총블럭의 수 구하기
+		Long perBlock=5L;//블럭당 번호의 갯수
+		Long totalBlock=0L;
+		totalBlock=totalPage/perBlock;
+		if(totalPage%perBlock != 0) {
+			totalBlock++;
+		}
+		//3. Page 값으로 현재 블럭 번호 구하기
+		Long curBlock=0L;//블럭 번호
+		curBlock=this.getPage()/perBlock;
+		if(this.getPage()%perBlock != 0) {
+			curBlock++;
 		}
 		
-		Long nowBlock =0L;
-		nowBlock = this.getPage()/perBlock;
-		if(this.getPage()%perBlock!=0) {
-		nowBlock++;
-		}
+		//4. 현재 블럭 번호로 시작 번호와 끝번호 구하기
+		Long startNum=0L;
+		Long lastNum=curBlock*perBlock;
+		startNum=lastNum-perBlock+1;
 		
+		this.setStartNum(startNum);
+		this.setLastNum(lastNum);
 		
-		Long lastNum= nowBlock*perBlock;
-		Long startNum=lastNum-perBlock+1;
-		 this.setStartNum(startNum);
-		 this.setLastNum(lastNum);
-		 
-		if(nowBlock==1) {
+		//이전, 다음 블럭 유무
+		if(curBlock==1) {
 			this.setStart(true);
 		}
-		if(nowBlock==totalBlock) {
+		
+		if(curBlock==totalBlock) {
 			this.setLastNum(totalPage);
 			this.setLast(true);
 		}
-		 
+	}
 
-	}
 	
-	public Long getStartRow() {
-		return startRow;
-	}
-	public void setStartRow(Long startRow) {
-		this.startRow = startRow;
-	}
-	public Long getLastRow() {
-		return lastRow;
-	}
-	public void setLastRow(Long lastRow) {
-		this.lastRow = lastRow;
-	}
+	//Getter
+	//public 리턴타입 get멤버변수명(){}
 	public Long getPage() {
-		if(this.page==null||this.page<1) {
+		if(this.page==null || this.page<1) {
 			this.page=1L;
 		}
-		return page;
+		return this.page;
 	}
-	public void setPage(Long page) {
-		this.page = page;
-	}
-
-
-
+	
 	public Long getPerPage() {
-		return perPage;
+		if(this.perPage==null || this.perPage<1) {
+			this.perPage=10L;
+		}
+		return this.perPage;
 	}
-
-
-
-	public void setPerPage(Long perPage) {
-		this.perPage = perPage;
-	}
-
-	public Long getTotalPage() {
-		return totalPage;
-	}
-
-	public void setTotalPage(Long totalPage) {
-		this.totalPage = totalPage;
-	}
-
-	public Long getStartNum() {
-		return startNum;
-	}
-
-	public void setStartNum(Long startNum) {
-		this.startNum = startNum;
-	}
-
-	public Long getLastNum() {
-		return lastNum;
-	}
-
-	public void setLastNum(Long lastNum) {
-		this.lastNum = lastNum;
-	}
-
-	public boolean isStart() {
-		return start;
-	}
-
-	public void setStart(boolean start) {
-		this.start = start;
-	}
-
-	public boolean isLast() {
-		return last;
-	}
-
-	public void setLast(boolean last) {
-		this.last = last;
-	}
-
+	
 	public String getSearch() {
 		if(this.search==null) {
 			this.search="";
 		}
-		return search;
+		
+		return this.search;
 	}
 
-	public void setSearch(String search) {
-		this.search = search;
-	}
-
-	public String getKind() {
-		return kind;
-	}
-
-	public void setKind(String kind) {
-		this.kind = kind;
-	}
-
-
-	
-	
-	
-	
 }
