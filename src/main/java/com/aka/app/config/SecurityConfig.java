20c74fb1979484827ab4d1ecbe0fc80d0d0d2d8f
gohyo04 <gohyo7@gmail.com>
2024-04-09 17:49:55 +0900

    0409_security_config

--- src/main/java/com/aka/app/config/SecurityConfig.java
+++ src/main/java/com/aka/app/config/SecurityConfig.java
@@ -1,33 +1,27 @@
 package com.aka.app.config;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import lombok.extern.slf4j.Slf4j;
+import com.aka.app.member.MemberService;
 
 @Configuration
 @EnableWebSecurity
-@Slf4j
 public class SecurityConfig {
 
-//	@Autowired
-//	private MemberService memberService;
+	@Autowired
+	private MemberService memberService;
 	
 	@Autowired
-	private SecurityLoginSuccessHandler loginSuccessHandler;
+	private SecurityLoginSuccessHandler securityLoginSuccessHandler;
 	
 	@Autowired
-	private SecurityLoginFailureHandler loginFailureHandler;
+	private SecurityLoginFailureHandler securityLoginFailureHandler;
 	
 	@Bean
 	WebSecurityCustomizer webSecurityCustomizer() throws Exception{
@@ -42,63 +36,28 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-	SecurityFilterChain filterChain (HttpSecurity security) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception{
 		
-		log.info("security config");
-		security
-				//CSRF 추가
-				.csrf(
-						(csrf) -> 
-									csrf.disable()
-				)
-				.authorizeHttpRequests(
-						(authorizeRequests)->
-											authorizeRequests
-											.anyRequest().permitAll()
-											)
-											
-				.formLogin(
-							(login)->
-									login	
-									.loginPage("/member/login")
-									.successHandler(loginSuccessHandler)
-									.usernameParameter("user_id") 
-									.defaultSuccessUrl("/")
-									.failureHandler(loginFailureHandler)
-									
-									.permitAll()
-
-									
-						)// 로그인 끝부분
-				.logout(
-							(logout)->
-									logout
-										.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-										.logoutSuccessUrl("/")
-										.invalidateHttpSession(true) // 로그아웃 성공시 session만료
-										.permitAll()
-						)// 로그아웃 끝 부분
-				.sessionManagement(
-						(sessionManagement)->
-							sessionManagement
-								.maximumSessions(1)
-								.maxSessionsPreventsLogin(false)
-								.expiredUrl("/expired")
-								
-				)//sessionManagement 끝
-				;	
-		
-		
-		
+		security.authorizeHttpRequests(
+				(authorizeHttpRequests) -> 
+					
+					authorizeHttpRequests
+					.requestMatchers("/").permitAll()
+			)
+			.formLogin(
+				(login) -> 
+					login
+						.loginPage("/login")
+						.successHandler(securityLoginSuccessHandler)
+						.failureHandler(securityLoginFailureHandler)
+						.permitAll()
+			);
+			
+			
 		return security.build();
 	}
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		// password 암호화 해주는 객체
-		return new BCryptPasswordEncoder();
-	}
 }
+
 
 
 
--- src/main/java/com/aka/app/config/SecurityLoginFailureHandler.java
+++ src/main/java/com/aka/app/config/SecurityLoginFailureHandler.java
@@ -1,64 +1,22 @@
 package com.aka.app.config;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 
-import javax.security.auth.login.CredentialExpiredException;
-
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.CredentialsExpiredException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.DatabindException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 
-@Component
-@Slf4j
 public class SecurityLoginFailureHandler implements AuthenticationFailureHandler{
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException exception) throws IOException, ServletException {
-		log.info("로그인 실패");
+		// TODO Auto-generated method stub
 		
-		String message = "로그인 실패";
-		
-		if(exception instanceof AuthenticationException) {
-			message = "계정유효기간이 만료되었습니다. (아이디 없음).";
-		}
-		
-		if(exception instanceof BadCredentialsException) {
-			message = "비밀번호가 틀립니다.";
-		}
-		
-		if(exception instanceof InternalAuthenticationServiceException) {
-			message = "존재하지않는 아이디 입니다.";
-		}
-			
-		if(exception instanceof LockedException) {
-			message = "계정이 잠겨있습니다.";
-		}
-		
-		if(exception instanceof CredentialsExpiredException) {
-			message = "비밀번호 유효기간이 종료되었습니다.";
-		}
-		
-		if(exception instanceof DisabledException) {
-			message = "휴면계정입니다.";
-		}
-		
-		message = URLEncoder.encode(message, "UTF-8");
-		
-		// 로그인 실패시 메세지를 파라미터로 넣어서 redirect
-		response.sendRedirect("/member/login?message"+message);
 	}
+
+	
 }
