8a378b31c563fa21ebf7c10ce6cf4d4d5664069d
Gohyo04 <gohyo7@gmail.com>
2024-04-10 23:03:17 +0900

    0410

--- src/main/java/com/aka/app/config/SecurityConfig.java
+++ src/main/java/com/aka/app/config/SecurityConfig.java
@@ -40,10 +40,10 @@ public class SecurityConfig {
 	private MemberService memberService;
 	
 	@Autowired
-	private SecurityLoginSuccessHandler securityLoginSuccessHandler;
+	private SecurityLoginSuccessHandler loginSuccessHandler;
 	
 	@Autowired
-	private SecurityLoginFailureHandler securityLoginFailureHandler;
+	private SecurityLoginFailureHandler loginFailureHandler;
 	
 	@Bean
 	WebSecurityCustomizer webSecurityCustomizer() throws Exception{
@@ -128,12 +128,30 @@ public class SecurityConfig {
 				(login) -> 
 					login
 						.loginPage("/login")
-						.successHandler(securityLoginSuccessHandler)
-						.failureHandler(securityLoginFailureHandler)
+						.loginProcessingUrl("/") 				// 인증처리 수행
+						.successHandler(loginSuccessHandler)	// 정상 인증 성공후 커스텀 핸들러
+						.failureHandler(loginFailureHandler)	// 인증 실패 후 커스텀 핸들러
+//						.failureUrl("/login/fail")				// 로그인 실패시 url (default 는 로그인 페이지)
 						.permitAll()
+			)
+			.logout(
+					(logout ->
+						logout
+							.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+							.logoutSuccessUrl("/")					// 로그아웃 후 url
+							.invalidateHttpSession(true)			// 세션만료
+							.permitAll()
+					)
+					
 			);
 			
 			
 		return security.build();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		// pw 암호화 객체
+		return new BCryptPasswordEncoder();
 	}
 }
