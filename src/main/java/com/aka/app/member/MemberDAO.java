253fa042d87c39ae6227155b8d28c7e08edb5e77
dirokim <dirokim7@gmail.com>
2024-04-15 19:13:50 +0900

    Revert "Merge branch 'develop' into feature-EQUIPMENT"
    This reverts commit 603cfa452c8790b44323b69a66ba85952657e87d, reversing
    changes made to 65843c9d8cfd17ccb2f92499990e04419986c765.

--- src/main/java/com/aka/app/member/MemberDAO.java
+++ src/main/java/com/aka/app/member/MemberDAO.java
@@ -3,9 +3,6 @@ package com.aka.app.member;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface MemberDAO {
-	
-	public MemberVO getDetail(MemberVO memberVO) throws Exception;
-	
-	public int add(MemberVO memberVO) throws Exception;
+public class MemberDAO {
+
 }
