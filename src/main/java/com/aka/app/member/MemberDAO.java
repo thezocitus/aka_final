03871026acba1d45fd8ea112f609d320f2cb898a
KimSeungkyun <dirokim7@gmail.com>
2024-04-16 17:05:42 +0900

    fix develop

--- src/main/java/com/aka/app/member/MemberDAO.java
+++ src/main/java/com/aka/app/member/MemberDAO.java
@@ -3,6 +3,9 @@ package com.aka.app.member;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class MemberDAO {
-
+public interface MemberDAO {
+	
+	public MemberVO getDetail(MemberVO memberVO) throws Exception;
+	
+	public int add(MemberVO memberVO) throws Exception;
 }
