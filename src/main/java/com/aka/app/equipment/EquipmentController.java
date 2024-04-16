f8ea727c2a27c9c37ce8e6a18d6f799195acf172
dirokim <dirokim7@gmail.com>
2024-04-16 09:09:49 +0900

    feat transactional

--- src/main/java/com/aka/app/equipment/EquipmentController.java
+++ src/main/java/com/aka/app/equipment/EquipmentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@Transactional
 @RequestMapping("/equipment/*")
 public class EquipmentController {
 	
